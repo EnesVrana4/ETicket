5a7c596796e103d218826b5ba8a724708bd6054f
EnesVrana4 <vranaeni4@gmail.com>
2022-10-05 15:44:49 +0200

    up to date

--- eTicketServices/DependencyInjection.cs
+++ eTicketServices/DependencyInjection.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eTicketServices.IServices;
+using eTicketServices.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace eTicketServices
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddETicketServices(this IServiceCollection services) {
+            services.AddScoped<IEventService, EventsService>();
+            return services;
+        }
     }
 }
